72b96d3956d26af5895a06b8a0359916914638ae
min <minhazur9@gmail.com>
2020-10-23 21:00:59 -0400

    found carmen

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -18,11 +18,12 @@ LIMIT
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 SELECT
-    name,
-    countrycode,
-    region
+    countrylanguage.language,
+    countrylanguage.countrycode,
+    country.region
 FROM
-    countrylanguage
+    country
+    INNER JOIN countrylanguage ON country.code = countrylanguage.countrycode
 WHERE
     countrycode = 'VAT';
 
@@ -39,7 +40,7 @@ FROM
     INNER JOIN countrylanguage ON country.code = countrylanguage.countrycode
 WHERE
     countrylanguage.language = 'Italian'    
-    AND countrylanguage.percentage = 100 
+    AND countrylanguage.percentage = 100;
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
 -- There are only two cities she could be flying to in the country. One is named the same as the country – that
 -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
@@ -56,16 +57,16 @@ WHERE
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
--- SELECT
---     country.name,
---     country.code,
---     country.region
--- FROM
---     city
---     INNER JOIN country ON city.countrycode = country.code
--- WHERE
---     country.region = 'South America'
---     AND city.name LIKE 'Serra%';
+SELECT
+    country.name,
+    country.code,
+    country.region
+FROM
+    city
+    INNER JOIN country ON city.countrycode = country.code
+WHERE
+    country.region = 'South America'
+    AND city.name LIKE 'Serra%';
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
