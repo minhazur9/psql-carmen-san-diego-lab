bd72cdbf455c31a00b17ce3a0e321e192618e3cc
min <minhazur9@gmail.com>
2020-10-23 20:39:18 -0400

    clue 6  done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -53,30 +53,30 @@
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-SELECT
-    country.name,
-    country.code,
-    country.region
-FROM
-    city
-    INNER JOIN country ON city.countrycode = country.code
-WHERE
-    country.region = 'South America'
-    AND city.name LIKE 'Serra%';
+-- SELECT
+--     country.name,
+--     country.code,
+--     country.region
+-- FROM
+--     city
+--     INNER JOIN country ON city.countrycode = country.code
+-- WHERE
+--     country.region = 'South America'
+--     AND city.name LIKE 'Serra%';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
+
 SELECT
     city.name,
     city.countrycode,
     city.district
 FROM
-    city
-    INNER JOIN country ON city.countrycode = countrycode
+    city,country
 WHERE
-    country.capital = city.id
-    and city.countrycode = 'BRA';
+    city.countrycode = 'BRA'
+    AND city.id = country.capital;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
